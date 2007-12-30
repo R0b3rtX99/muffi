c5a90586db379eed4c2795e9acf6624e91e0b507
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2007-12-30 06:13:45 +0000

    Created a test folder which will contain test harnesses I will use to make sure we aren't munging our code too badly.

---
+++ tests/basetest.py
@@ -0,0 +1,13 @@
+'''
+This is just a quick test to make sure the
+core components are being initialized correctly.
+'''
+
+from immlib import *
+from muffi  import *
+
+def main(args):
+    
+    # Just instantiate muffi
+    
+    return "Muffi Test Completed"
