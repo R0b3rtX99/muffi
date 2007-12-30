cdad61a8ce7fe571410f16a331e859b3c18d3c89
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2007-12-30 18:48:41 +0000

    Adding a placeholder class for exception handling. Still have to implement some of the finer details.

---
+++ mfx.py
@@ -0,0 +1,41 @@
+#
+# muffi - Malware and Unpacking Framework for Immunity Debugger
+# 
+# Copyright (C) 2007 Justin Seitz <jms@bughunter.ca>, BoB <BobSoft@GMail.Com>
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+'''
+@author:       Justin Seitz, BoB
+@license:      GNU General Public License 2.0 or later
+@contact:      jms@bughunter.ca, bobsoft@gmail.com
+'''
+
+from ctypes         import *
+from immlib         import *
+from muffi_defines  import *
+
+
+class mfx(Exception):
+    '''
+    This is a generic exception class, that allows
+    you to raise a custom exception as well as trap
+    Win32 exceptions and output them in a friendly
+    format. This is loosely based on the PyDbg pdx
+    class.
+    '''
+    
+    def __init__(self,message,win32_error = False):
+        imm = Debugger()
+        
+        imm.Log("[*] %s" % message)
+        
