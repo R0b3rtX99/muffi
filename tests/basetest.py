81154b3050ae27822efbf8654328627cf3e49711
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2007-12-30 07:43:14 +0000

    Added and passed poly_eax_zero test.

--- tests/basetest.py
+++ tests/basetest.py
@@ -6,14 +6,50 @@ core components are being initialized correctly.
 from immlib import *
 from muffi  import *
 
+def test_poly_eax_dword(imm,mf):
+    imm.Log("[Test] poly_eax_dword:")
+        
+    instructions = mf.patch_utils.poly_eax_dword(value=0)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_dword()
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_dword(flavor=1)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_dword(flavor=2)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_dword(flavor=3)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_dword(flavor=4)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX")) 
 
-def main(args):
+def test_poly_eax_zero(imm,mf):
+    imm.Log("[Test] poly_eax_zero:")
     
+    instructions = mf.patch_utils.poly_eax_zero()
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_zero(flavor=1)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_zero(flavor=2)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_zero(flavor=3)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    instructions = mf.patch_utils.poly_eax_zero(flavor=4)
+    imm.Log("[Test] Output: %s" % instructions.encode("HEX"))
+    
+    
+def main(args):
+    imm = Debugger()
     
     # Just instantiate muffi
     mf = muffi()
     
+    # Call the default harnesses
     mf.anti_debug.harness()
     mf.patch_utils.harness()
+    
+    # Add test cases here in the form of test_[function_name]
+    # Your test function should take imm, and mf instances for parameters
+    test_poly_eax_dword(imm,mf)
+    test_poly_eax_zero(imm,mf)
     
     return "Muffi Test Completed"
