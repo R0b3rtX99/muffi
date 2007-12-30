a8ea5518bc34f4bbe378f3b7b702ca7566e75ae6
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2007-12-30 06:15:38 +0000

    Committing the first run at anti_debug, patch_utils and the base muffi class.

---
+++ anti_debug.py
@@ -0,0 +1,51 @@
+#
+# muffi - Malware and Unpacking Framework for Immunity Debugger
+# 
+# Copyright (C) 2007 Justin Seitz <jms@bughunter.ca>, BoB <BobSoft@GMail.Com>
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+'''
+@author:       Justin Seitz, BoB
+@license:      GNU General Public License 2.0 or later
+@contact:      jms@bughunter.ca, bobsoft@gmail.com
+'''
+import struct
+import random
+import ctypes
+
+from patch_utils import *
+from immlib      import *
+
+class anti_debug():
+    '''
+    This class encapsulates all anti-debugging tricks, you can call
+    individual methods to apply a particular patch or you can use 
+    anti_debug.all() to apply all developed anti-debugging bypasses.
+    Unless specifically stated otherwise, all of these tricks are by BoB 
+    from team PEid.
+    '''
+    
+    def __init__(self):
+        
+        self.imm            =    Debugger()
+    
+    def harness(self):
+        '''
+        Standard harness for testing new functionality
+        as we build it. This is kept in the final release
+        so that developers can test their patches before
+        we commit them.
+        '''
+        
+        self.imm.Log("Anti-Debug harness function called.")
+        
