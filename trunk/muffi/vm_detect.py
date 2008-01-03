a10a7c7451c13637b64160bc2677165e6fc39d14
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2008-01-03 18:50:17 +0000

    Storing first iteration of virtual machine cloaking.

--- trunk/muffi/vm_detect.py
+++ trunk/muffi/vm_detect.py
@@ -38,8 +38,12 @@ class vm_detect():
     def __init__(self):
         
         self.imm        =    Debugger()
+        self.sidt_list  =    []            # List of SIDT instruction addresses
+        self.sldt_list  =    []            # List of SLDT instruction addresses
+        self.sgdt_list  =    []            # List of SGDT instruction addresses
+        self.os         =    None          # Operating system to spoof to the executable
         
-    def cloak_vmware(self):
+    def cloak_vmware(self,os=None):
         """
         This function is responsible for finding traces of
         VMWare(c) detection code in the debuggee. It will
@@ -47,8 +51,70 @@ class vm_detect():
         the virtual machine is hidden. Future copies should 
         also hook all registry checks, file checks etc.
         
+        @type: os    String
+        @param os    (Optional)The operating system you wish to spoof, 
+        defaults to randomly picking one. The options are::
+        
+            WindowsXP
+            Windows2000
+        
         @raise: mfx An exception is raised if the cloaking fails.
         @rtype: Boolean
         @return: Returns True if the patches were applied correctly.
+        
+        @see: dt_search()
         """
-        pass
+        if os.lower() not in ("WindowsXP".lower(),"Windows2000".lower()):
+            raise mfx("The operating system must be one of WindowsXP or Windows2000")
+        else:
+            self.os    =    os
+        
+        # First find any SIDT, SGDT and SLDT instructions
+        # and hook them so that we can modify the return value
+        dt_matches = self.dt_search()
+    
+    def dt_search(self):
+        """
+        This is a helper function for cloaking VMWare. It locates
+        any SIDT, SGDT and SLDT functions, so that we can install
+        hooks into them.
+        
+        @raise: mfx An exception is raised if the search fails.
+        @rtype: List
+        @return: Returns a list of all the matches.
+        """
+        
+        self.sidt_list = self.imm.Search(SIDT_OPCODE)
+        self.sgdt_list = self.imm.Search(SGDT_OPCODE)
+        self.sldt_list = self.imm.Search(SLDT_OPCODE)
+        
+        
+        # Now let's build some hooks 
+        dt_hooker = dt_hooks()
+    
+    
+ 
+class dt_hooks(LogBpHook):
+    """
+    This is a helper class that is responsible for
+    hooking the various instructions that are responsible
+    for storing descriptor table addresses.
+    """
+    
+    def __init__(self,hook_register,operating_system):
+        """
+        This just sets up the hook itself, and initializes 
+        a debugger instance.
+        
+        @type:  hook_register String
+        @param: hook_register The register that we need to modify with our own table entry.
+        
+        """
+        LogBpHook.__init__(self)
+        self.imm        =    Debugger()
+    
+    def run(self,regs):
+        """
+        This is run when the hook gets hit. It is responsible
+        for writing a 
+        
