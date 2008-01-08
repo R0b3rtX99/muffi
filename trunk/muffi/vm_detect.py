25607f5a21a14636646ce1ff710ebf9ddcc563cd
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2008-01-08 00:54:31 +0000

    First somewhat working copy of a VM cloaking mechanism. Still working on tracking down an error when doing the returning JMP from our detour code page.

--- trunk/muffi/vm_detect.py
+++ trunk/muffi/vm_detect.py
@@ -77,11 +77,16 @@ class vm_detect():
     
         # First find any SIDT, SGDT and SLDT instructions
         # and hook them so that we can modify the return value
-        self.dt_search()
-    
-        # Now let's build some hooks 
-        dt_hooker = dt_hooks(self.hook_addrs,self.os)
+        self.imm.Log("About to search")
+        search = self.dt_search()
+
+        if search != False:
+            # Now iterate through the found addresses and 
+            # patch the code so that it sets the appropriate
+            # descriptor constant
+            self.dt_patch()
         
+                  
     def dt_search(self):
         """
         This is a helper function for cloaking VMWare. It locates
@@ -93,65 +98,141 @@ class vm_detect():
         @return: Returns a list of all the matches.
         """
         
-        self.sidt_list = self.imm.Search(SIDT_OPCODE)
-        self.sgdt_list = self.imm.Search(SGDT_OPCODE)
-        self.sldt_list = self.imm.Search(SLDT_OPCODE)
+        # Grab the module information and make sure the code is
+        # analysed
+        module = self.imm.getModule(self.imm.getDebuggedName())
         
-        # Determine the register we want at hook-time
-        for address in self.sidt_list:
-            opcode        = self.imm.disasm(address)
-            hook_register = opcode.getDisasm().split("[")[1].replace("]","") 
-            self.hook_addrs[address] = (hook_register)
+        if not module.isAnalysed():
+            self.imm.analyseCode(module.getCodebase())
         
-        for address in self.sgdt_list:
-            opcode        = self.imm.disasm(address)
-            hook_register = opcode.getDisasm().split("[")[1].replace("]","") 
-            self.hook_addrs[address] = (hook_register)
+        # Calculate how much of the binary we need to walk through
+        code_base = module.getCodebase()
+        code_len  = code_base + module.getCodesize()
+        current_ea= module.getCodebase()
         
-        for address in self.sldt_list:
-            opcode        = self.imm.disasm(address)
-            hook_register = opcode.getDisasm().split("[")[1].replace("]","") 
-            self.hook_addrs[address] = (hook_register)
-        
-        
-        return True
-
-
-class dt_hooks(LogBpHook):
-    """
-    This is a helper class that is responsible for
-    hooking the various instructions that are responsible
-    for storing descriptor table addresses.
-    """
-    
-    def __init__(self,hook_addrs,os):
-        """
-        This just sets up the hook itself, and initializes 
-        a debugger instance.
-        """        
-        LogBpHook.__init__(self)
-        self.imm            =    Debugger()
-        self.hook_addrs     =    hook_addrs
-        self.os             =    os
-       
-        # Iterate through all of the addresses, and set hookss
-        for address in self.hook_addrs:
-            self.imm.Log("Hook: %08x" % address)
-            self.add("%08x" % address,address)
-            self.imm.addKnowledge("%08x" % address,"%08x|%s" % (address,self.hook_addrs[address]))
+        # Start walking through the binary looking for those
+        # particular calls
+        found_call    =    False
+        while current_ea <= code_len:
+            current_opcode = self.imm.disasmForward(current_ea)
             
+            current_ea = current_opcode.getAddress()
+            
+            inst = current_opcode.getDisasm()[:4]
+            
+            if inst == "SIDT":
+                self.imm.Log("Found it!",address = current_opcode.getAddress())
+                self.hook_addrs[current_ea] = ("idt",current_opcode)
+                found_call = True
+            elif inst == "SLDT":
+                self.imm.Log("Found it!",address = current_opcode.getAddress())
+                self.hook_addrs[current_ea] = ("ldt",current_opcode)
+                found_call = True
+            elif inst == "SGDT":
+                self.imm.Log("Found it!",address = current_opcode.getAddress())
+                self.hook_addrs[current_ea] = ("gdt",current_opcode)
+                found_call = True
         
-    def run(self,regs):
+        return found_call        
+        
+        
+    def dt_patch(self):
         """
-        This is run when the hook gets hit. It is responsible
-        for writing out the constant that will trick
-        the process into thinking it's running on a host and 
-        not a guest.
+        This function is responsible for patching the instructions
+        that are issuing descriptor table address lookups. We simply
+        do a MOV [R32], 0xCONSTANT at the address of the instruction.
+        
+        @raise:     mfx     An error is raised if we can't patch the instructions.
+        @rtype:     Boolean
+        @return:    True if the patches were applied successfully.
         """
+                
+        if self.os.lower() == "windowsxp":
+            idt     =    WINXP_2003_IDT
+            ldt     =    WINXP_2003_LDT
+            gdt     =    WINXP_2003_GDT
+        else:
+            idt     =    WIN2000_IDT
+            ldt     =    WIN2000_LDT
+            gdt     =    WIN2000_GDT
         
-        # We need to figure out where the instruction
-        # is pointing
-        register = self.imm.getKnowledge("%08x" % regs['EIP'])
-        self.imm.Log("This pile is here",address = regs['EIP'])
-        
-        return True
+        # Iterate through the addresses, and write 
+        # the trickery
+        for address in self.hook_addrs:
+            
+            # Create the stub where we will detour
+            stub_address = self.imm.remoteVirtualAlloc(size=50)
+
+            opcode = self.hook_addrs[address][1]
+                        
+            # Determine what we have to patch
+            if self.hook_addrs[address][0] == "idt":
+                patch_value = idt
+            elif self.hook_addrs[address][0] == "gdt":
+                patch_value = gdt
+            elif self.hook_addrs[address][0] == "ldt":
+                patch_value = ldt
+            
+                        
+            # First we need to determine the length of the original
+            # opcode, if the length is greater than 5 bytes, we need to 
+            # preserve the instruction that follows it, the reason is
+            # our JMP [ALLOC_ADDRESS] is 5 bytes long
+            saved_instructions    =    None
+            nop_sled              =    None
+            
+            if opcode.getSize() < 5:
+                new_opcode = self.imm.disasmForward(address)
+                saved_instructions = new_opcode.getDisasm()
+                self.imm.Log("The length is going ot be an issue: %d" % opcode.getSize(), address = address)
+                self.imm.Log("About to clobber: %s" % new_opcode.getDisasm(),address = new_opcode.getAddress())
+            
+                # It seems to be easier to NOP everything out first before
+                # we write out the patch
+                nop_sled = "\x90" * (opcode.getSize() + new_opcode.getSize())
+                self.imm.writeMemory(address,nop_sled)
+                
+            # Now let's write the detour JMP to our allocated memory page
+            detour_jmp     = self.imm.Assemble("JMP 0x%08x" % stub_address,address = address)
+            detour_jmp_len = len(detour_jmp)
+            
+            self.imm.writeMemory(address,detour_jmp)
+            
+            # Now write out the patch header, we are just going to do a MOV on
+            # the original SIDT/SLDT/SGDT instruction, directly into it's operand
+            operand = opcode.getDisasm()[5:]
+            register = operand.split("[")[1].split("]")[0]
+            
+            if opcode.getMemType() == MEM_TYPE_WORD and patch_value == ldt:
+                patch_header = "MOV WORD PTR [%s],0x0000 \n" % register
+            else:
+                patch_header = "MOV DWORD PTR [%s],0x%08x \n" % (register,patch_value)
+            
+            
+            # Now if we need to preserve some instructions from the original
+            # basic block (because we clobbered them with our detour JMP)
+            # write them out after the patch header
+            if saved_instructions is not None:
+                patch_body = new_opcode.getDisasm() + "\n"
+            else:
+                patch_body = "\n"
+
+            # Write the return address
+            # Now we want to do the JMP back to the original function
+            # plus the size of our detour JMP instruction
+            if nop_sled is not None:
+                detour_ret = address + len(nop_sled)
+            else:
+                detour_ret = address + detour_jmp_len
+                
+            self.imm.Log("Detour Return: 0x%08x" % detour_ret)
+            ret_jmp = "JMP 0x%08x \n" % detour_ret
+            
+            
+            
+            # Assemble the final patch
+            final_patch = self.imm.Assemble(patch_header + patch_body + ret_jmp, address = stub_address)
+            self.imm.Log("Final Patch: %s" % final_patch.encode("HEX"))
+            self.imm.writeMemory(stub_address,final_patch)
+          
+            
