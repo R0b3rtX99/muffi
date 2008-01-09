dc8a3e68c1c7e32b8473a0f6c560594f3f69fc8c
justin.seitz <justin.seitz@c058eafa-4441-0410-94c9-e397ae3e92bd>
2008-01-09 00:21:09 +0000

    Just added the vm_detect module to its path.

--- branches/templates/build_docs.bat
+++ branches/templates/build_docs.bat
@@ -1,1 +1,1 @@
-c:\python25\python c:\python25\lib\site-packages\epydoc\cli.py --url http://muffi.googlecode.com/ -v --html --name muffi -o docs --parse-only muffi.py patch_utils.py anti_debug.py
+c:\python25\python c:\python25\lib\site-packages\epydoc\cli.py --url http://muffi.googlecode.com/ -v --html --name muffi -o docs --parse-only muffi.py patch_utils.py anti_debug.py vm_detect.py
